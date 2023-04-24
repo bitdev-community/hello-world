94a59330fdadaddcb98698ff73a7c4c2c282652e
ashanfernando <ashan256@gmail.com>
2023-04-24 16:09:55 +0530

    Adding netlify deployer support

--- apps/hello-world-app/hello-world-app.react-18-app.cjs
+++ apps/hello-world-app/hello-world-app.react-18-app.cjs
@@ -1,5 +1,17 @@
 /** @type {import("@teambit/react.apps.react-app-types").ReactAppType} */
+import {
+  Netlify,
+  NetlifyOptions,
+} from '@teambit/cloud-providers.deployers.netlify';
+
+const netlifyConfig = {
+  accessToken: process.env.NETLIFY_AUTH_TOKEN,
+  productionSiteName: 'your-site-name',
+  team: 'your-team-name',
+};
+
 module.exports.default = {
   name: 'hello-world-app',
   entry: [require.resolve('./hello-world-app.app-root')],
+  deploy: netlify.deploy.bind(netlify),
 };
