d9cdb1f07055123a7d71a4090c02f276f5200fae
ashanfernando <ashan256@gmail.com>
2023-04-25 19:26:17 +0530

    Fixing netlify deployer issue and ssh issue

--- apps/hello-world-app/hello-world-app.react-18-app.cjs
+++ apps/hello-world-app/hello-world-app.react-18-app.cjs
@@ -1,14 +1,13 @@
 /** @type {import("@teambit/react.apps.react-app-types").ReactAppType} */
-import {
-  Netlify,
-  NetlifyOptions,
-} from '@teambit/cloud-providers.deployers.netlify';
+const { Netlify } = require('@teambit/cloud-providers.deployers.netlify');
 
 const netlifyConfig = {
   accessToken: process.env.NETLIFY_AUTH_TOKEN,
   productionSiteName: 'your-site-name',
   team: 'your-team-name',
 };
+
+const netlify = new Netlify(netlifyConfig);
 
 module.exports.default = {
   name: 'hello-world-app',
